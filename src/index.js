7a1a63ab6d363deb391bed0dff69692a20f3a628
michaladamiak <mchadamiak@gmail.com>
2022-04-04 19:11:00 +0200

    variables

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //creating different boards for each size chosen by a player
     const pairs = []
-    const random_indexes = []
+    const randomIndexes = []
     const indexes = []
-    let length_of_side = 0
+    let lengthOfSide = 0
     let size = 0
     Array.from(options).forEach(element => {
-        length_of_side+=2
-        element.square = length_of_side
+        lengthOfSide+=2
+        element.square = lengthOfSide
         element.addEventListener('click', function(){
             board.style.display = 'flex'
             container.style.display = 'none'
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 iterations++
             }
             pairs[i] = 'pair' + iterations
-            random_indexes[i]=i
+            randomIndexes[i]=i
             indexes[i] = document.createElement('div')
             board.appendChild(indexes[i])
             indexes[i].appendChild(document.createElement('div'))
@@ -48,28 +48,28 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // array of random_indexes contains all posible indexes of board, when specific element of random_indexes is randomly selected it gets erased from array, when random_indexes is finnaly empty, all cards have their spot
+    // array of randomIndexes contains all posible indexes of board, when specific element of randomIndexes is randomly selected it gets erased from array, when randomIndexes is finnaly empty, all cards have their spot
 
     function createPairs(size) {
         // passing created classes to random divs on board 
         let color = ''
-        let spots_left = size*size
+        let spotsLeft = size*size
         for(let i=0; i<size*size; i++) {
             //each pair of cards gets the same random color
             if(i%2==0){
                 color = 'rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')'
             }
             //getting random index
-            random_index = Math.floor(Math.random()*spots_left)
-            spots_left--
+            randomIndex = Math.floor(Math.random()*spotsLeft)
+            spotsLeft--
             //every two elements get the same class which is used to comparison
-            indexes[random_indexes[random_index]].className = pairs[i]
+            indexes[randomIndexes[randomIndex]].className = pairs[i]
             //passing colors
-            indexes[random_indexes[random_index]].children[0].children[1].style.backgroundColor = color
-            //deleting drawn index from random_indexes
-            for(let j=0; j<random_indexes.length; j++){ 
-                if ( random_indexes[j] == random_indexes[random_index]) { 
-                    random_indexes.splice(j, 1); 
+            indexes[randomIndexes[randomIndex]].children[0].children[1].style.backgroundColor = color
+            //deleting drawn index from randomIndexes
+            for(let j=0; j<randomIndexes.length; j++){ 
+                if ( randomIndexes[j] == randomIndexes[randomIndex]) { 
+                    randomIndexes.splice(j, 1); 
                 }
             }
         }
@@ -97,7 +97,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let kliks = 0
     let choice1 = NaN
     let choice2 = NaN
-    let prev_choice = NaN
+    let prevChoice = NaN
     let points = 0
     function choosing(event){
         event.target.parentElement.style.transform = 'rotateY(-180deg)'
@@ -108,22 +108,22 @@ document.addEventListener('DOMContentLoaded', () => {
             //check which pair this card belongs to
             choice1 = event.target.parentElement.parentElement.className
             //remember this choice for seceond choice to compair 
-            prev_choice = event.target.parentElement.parentElement
+            prevChoice = event.target.parentElement.parentElement
         //second card selected
         } else {
             //check if player selected 2 different cards
-            if(event.target.parentElement.parentElement != prev_choice) {
+            if(event.target.parentElement.parentElement != prevChoice) {
                 //check which pair this card belongs to
                 choice2 = event.target.parentElement.parentElement.className
                 //if cards belong to same different classes rotate them
                 if(choice1!=choice2) {
                     setTimeout(wrongChoice, 750, event.target.parentElement)
-                    setTimeout(wrongChoice, 750, prev_choice.children[0])
+                    setTimeout(wrongChoice, 750, prevChoice.children[0])
                 //if calds belong to same class remove event listener and add point
                 } else {
                     points++
                     event.target.parentElement.removeEventListener('click', choosing)
-                    prev_choice.children[0].removeEventListener('click', choosing)
+                    prevChoice.children[0].removeEventListener('click', choosing)
                 }
             //if player selected same card twice rotate it
             } else {
